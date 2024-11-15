cf341d643cb32d8abe75af226b89fb85571cd053
Angelo <angvil02@gmail.com>
2024-11-14 16:05:40 -0800

    fixed submission for getAllReservations

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -101,13 +101,14 @@ const getAllReservations = function (guest_id, limit = 10) {
       `
       SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
       FROM reservations
-      JOIN properties ON properties.id = reservations.property_id
+      LEFT JOIN properties ON properties.id = reservations.property_id
       JOIN property_reviews ON property_reviews.property_id = properties.id
       WHERE reservations.guest_id = $1
       GROUP BY reservations.id, properties.id
       ORDER BY start_date
+      LIMIT $2
       `,
-      [guest_id]
+      [guest_id, limit]
     )
     .then((res) => {
       return(res.rows);
